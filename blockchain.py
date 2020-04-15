b573d29fac89628eb717ceb0623a9b583b76db7b
hunandres96 <hunandres96@hotmail.com>
2020-04-15 16:49:24 -0500

    added function to get the last block from the chain

--- blockchain.py
+++ blockchain.py
@@ -20,9 +20,11 @@ class Blockchain:
                     'proof': proof,
                     'previous_hash': previous_hash
                 }
-        
         self.chain.append(block)
         return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
 
 
 # part 2 - mining our blockchain
