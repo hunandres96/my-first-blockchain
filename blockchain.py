12fb4f2d3f8d09f5345d7fa1aecfca39222db49d
hunandres96 <hunandres96@hotmail.com>
2020-04-28 11:40:31 -0500

    added transaction key to the response of the mine_block request

--- blockchain.py
+++ blockchain.py
@@ -100,6 +100,9 @@ class Blockchain:
 # creating a web app
 app = Flask(__name__)
 
+# create address for node on port 5000
+node_address = str(uuid4()).replace('-', '')
+
 # creating a blockchain
 blockchain = Blockchain()
 
@@ -110,13 +113,15 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transactions(sender = node_address, receiver = 'Andres', amount=1)
     block = blockchain.create_block(proof, previous_hash)
     response = {
             'message': 'Congratulations, you just mined a block',
             'index': block['index'],
             'timestamp': block['timestamp'],
             'proof': block['proof'],
-            'previous_hash': block['previous_hash']
+            'previous_hash': block['previous_hash'],
+            'transactions': block
             }
     return jsonify(response), 200
     
